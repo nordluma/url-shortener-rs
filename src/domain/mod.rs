159fd90c6022970f4900ec589d79ad5bb1bf55ac
nordluma <maxnordlund.95@hotmail.com>
2024-02-24 16:51:41 +0200

    feat: create module for domain logic

---
+++ src/domain/mod.rs
@@ -0,0 +1,25 @@
+pub mod get;
+pub mod post;
+
+pub use get::*;
+pub use post::*;
+
+pub struct Url {
+    url: String,
+    created_at: String,
+    last_modified: String,
+    request_count: usize,
+}
+
+impl From<NewUrl> for Url {
+    fn from(value: NewUrl) -> Self {
+        let now = "NOW".to_string(),
+
+        Self {
+            url: value.url,
+            created_at: now,
+            last_modified: now,
+            request_count: 0,
+        }
+    }
+}
---
+++ src/domain/post.rs
@@ -0,0 +1,3 @@
+struct NewUrl {
+    url: String,
+}
