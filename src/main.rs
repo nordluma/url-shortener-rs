5ed5d6fdab18ff30bbc90704270c105a05ae1fde
nordluma <maxnordlund.95@hotmail.com>
2024-02-24 16:13:43 +0200

    feat: setup http server and serve homepage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = "127.0.0.1";
+    let port = 8080;
+
+    HttpServer::new(|| App::new().route("/", web::get().to(homepage)).route("/api/url", web::post().to(create_url)))
+        .bind((addr, port))?
+        .run()
+        .await?;
+
+    Ok(())
+}
+
+// TODO: serve homepage
+async fn homepage() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(include_str!("../static/index.html"))
+}
+
+// TODO: insert url into db and return shortened url
+async fn create_url() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world")
 }
