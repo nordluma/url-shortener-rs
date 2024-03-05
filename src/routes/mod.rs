54e39aa22f5c3b82659adf9a91e841d7ac2f34a9
nordluma <maxnordlund.95@hotmail.com>
2024-03-05 19:49:49 +0200

    fix(routes): the route should return all urls...

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -8,7 +8,7 @@ pub mod url;
 pub fn api_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::resource("/url")
-            .route(web::get().to(routes::url::get_url))
+            .route(web::get().to(routes::url::get_urls))
             .route(web::post().to(routes::url::create_url)),
     );
 }
